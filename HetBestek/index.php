ca9163d7d436a6af7479e10e7b025726613c53eb
Amber <amber.adrichem@gmail.com>
2023-11-17 09:52:46 +0100

    homepage problematiek :( hopelijk nu wel de header gefixt!!!

--- HetBestek/index.php
+++ HetBestek/index.php
@@ -17,10 +17,7 @@
 </head>
 <div class="header">
     <?php
-    include "../header/header.html";
-    ?>
-    <?php
-    include "../Algemeen/navigation menu.html"
+    include "../header/header.php";
     ?>
 
 <!--<div class="logo">
---
+++ header/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <link rel="stylesheet" href="header.css">
+
+    <header>
+        <?php include
+        "../Algemeen/navigation menu.html"
+        ?>
+        <link rel="stylesheet" href="../Algemeen/Hamburgers.css">
+        <div class="logo">
+            <img src="../Img/IMG_1915.png" alt="Nerdy Gadgets Logo" class="logo">
+        </div>
+    </header>
+</head>
+
+
+<body>
+</body>
+</html>
