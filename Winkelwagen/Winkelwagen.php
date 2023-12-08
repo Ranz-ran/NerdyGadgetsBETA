d270879bd85f416b700e1fb3c47ae9306b96e955
Rita <ritakuo2002@gmail.com>
2023-12-08 10:26:31 +0100

    code review

--- Winkelwagen/Winkelwagen.php
+++ Winkelwagen/Winkelwagen.php
@@ -6,10 +6,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../header/header.css">
-        <?php
-        include"../header/header.php"
-        ;?>
-<link rel="stylesheet" href="Winkelwagen.css">
+    <link rel="stylesheet" href="Winkelwagen.css">
+
+    <?php
+    include"../header/header.php";
+    ?>
+
 </head>
 <body>
 <div class="wrap">
