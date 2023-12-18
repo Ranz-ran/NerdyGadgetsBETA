8299c5df1ae239bf64a38b1fe4671b88823ea853
Troy <troy.stilting@st-2.com>
2023-12-18 14:38:11 +0100

    login button werkt

--- header/header.php
+++ header/header.php
@@ -23,10 +23,10 @@
                     $user_id = $_SESSION['user_id'];
                     $firstName = $_SESSION['naam'];
                     print("<p>".$firstName."</p>");
-                    print("<a href='' class='Button'>Sign Out</a>");
+                    print("<a href='../HetBestek/logout.php' class='Button'>Sign Out</a>");
                 } else {
-                    print("<a href='login.php' class='Button'>Login</a>");    
-                    print("<a href='signin.php' class='Button'>Sign In</a>");  
+                    print("<a href='../HetBestek/login.php' class='Button'>Login</a>");    
+                    print("<a href='../HetBestek/signin.php' class='Button'>Sign In</a>");  
                 }    
             ?>
         </div>
